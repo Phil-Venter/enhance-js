ac9b712c826850162be02166cb86e925a3fa1be0
Philip <Phil-Venter@users.noreply.github.com>
2020-01-10 12:40:12 +0200

    function to arrow function
    if it does not need context (this) I'm going to use arrow functions, looks cleaner in my opinion

--- array.js
+++ array.js
@@ -11,7 +11,7 @@
  *
  * @returns {number[]}
  */
-Array.range = function(start, end, step = 1) {
+Array.range = (start, end, step = 1) => {
     let returnArray = [];
     for (end || (end = start, start = 0); start <= end; start += step) returnArray.push(start);
     return returnArray;
