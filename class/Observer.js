e77d81519120341c47173796ff49a82a27519648
Philip <Phil-Venter@users.noreply.github.com>
2020-01-10 11:00:50 +0200

    Created Observer, Thanks Sheikh Irshad
    Sheikh Irshad PubSub implementation https://www.initgrep.com/posts/javascript/design-patterns/publisher-Subscriber(pub-sub)

---
+++ class/Observer.js
@@ -0,0 +1,55 @@
+/**
+ * @class Observer
+ *
+ * @description Very simple implementation of Observables,
+ *              Thanks Sheikh Irshad
+ *              {@link https://www.initgrep.com/posts/javascript/design-patterns/publisher-Subscriber(pub-sub)}
+ */
+class Observer {
+    /**
+     * @description Creates an instance of Observer.
+     *
+     * @memberof Observer
+     */
+    constructor() {
+        this.events = {};
+    }
+
+    /**
+     * @public subscribe
+     *
+     * @description create event and add listener
+     *
+     * @param {String} event
+     * @param {Function} listener
+     *
+     * @memberof Observer
+     */
+    subscribe(event, listener) {
+        if (!listener || typeof listener !== 'function')
+            throw new Error('listener was not a function');
+        if (!this.events[event] || this.events[event].length < 1)
+            this.events[event] = [];
+        this.events[event].push(listener);
+    }
+
+    /**
+     * @public publish
+     *
+     * @description pass data to events
+     *
+     * @param {String} event
+     * @param {Object} params
+     *
+     * @memberof Observer
+     */
+    publish(event, params) {
+        if (!this.events[event] || this.events[event].length < 1)
+            throw new Error('Event does not exist');
+        if (!params || typeof params !== 'object')
+            throw new Error('params was not an object');
+        this.events[event].forEach((listener) => {
+            listener(params);
+        });
+    }
+}
