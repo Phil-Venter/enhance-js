f691be78cf17f254855cdbce5a4d0495ec04d6ce
Philip <Phil-Venter@users.noreply.github.com>
2020-01-10 12:38:20 +0200

    It's always good to comment your code 🤷‍♂️

--- object.js
+++ object.js
@@ -1,3 +1,12 @@
+/**
+ * @public Object.extend
+ *
+ * @description a way combine objects and return a new immutable object
+ *
+ * @param {objects} args
+ *
+ * @return object
+ */
 Object.extend = (...args) => {
     return Object.freeze(Object.assign({}, ...args));
 }
